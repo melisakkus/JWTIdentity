82a800ffcf5b3b5ddcd12ef4ab23f7dfb34b9018
Melisa Akkuş <melisa.akkus01@gmail.com>
2025-04-15 22:14:56 +0300

    Add project files.

---
+++ JWTIdentity.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using JWTIdentity.API.Entities;
+using JWTIdentity.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTIdentity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(UserManager<AppUser> _userManager) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto model)
+        {
+            var user = new AppUser
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await _userManager.CreateAsync(user,model.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok("Kullanıcı Kaydı Başarılı");
+        }
+
+    }
+}
---
+++ JWTIdentity.API/Options/TokenOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace JWTIdentity.API.Options
+{
+    public class TokenOptions
+    {
+        public string Issuer { get; set; } //yayıncı kim
+        public string Audience { get; set; } //kullanıcı kim
+        public string Key { get; set; } //anahtar
+        public int ExpireInMinutes { get; set; } //yaşam süresi dakika
+
+    }
+}
